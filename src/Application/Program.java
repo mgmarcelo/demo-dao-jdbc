58d673dc1ebce36d9b2321a66495421e674c6c96
Marcelo Gonçalves <goncalves.marceloaugusto@gmail.com>
2019-11-26 00:28:06 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,13 +1,20 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(8, "Marcelo", "marcelo@gmail.com", new Date(),15000.0, obj);
+		
+		
+		System.out.println(seller);
 	}
 
 }
